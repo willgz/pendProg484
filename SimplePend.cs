b802a7d576f0253edc98e5bae95a0d9e115527fe
Will <wgzalez@outlook.com>
2022-01-31 09:51:24 -0600

    Initial commit for the file

---
+++ SimplePend.cs
@@ -0,0 +1,46 @@
+//============================================================================
+// SimplePend.cs    Defines a class for simulating a simple pendulum
+//============================================================================
+using System;
+
+namespace Sim
+{
+    public class SimplePend
+    {
+        private double len = 1.1; // pendulum length
+        private double g = 9.81; // gravitational field strength
+
+        //--------------------------------------------------------------------
+        // constructor
+        //--------------------------------------------------------------------
+        public SimplePend()
+        {
+            Console.WriteLine("Inside Constructor");
+        }
+
+        //--------------------------------------------------------------------
+        // Getters and setters
+        //--------------------------------------------------------------------
+        public double L
+        {
+            get{return(len);}
+
+            set
+            {
+                if (value > 0.0)
+                    len = value;
+            }
+        }
+        public double G
+        {
+            get{return(g);}
+
+            set
+            {
+                if (value >= 0.0)
+                    g = value;
+            }
+        }
+    }
+    
+}
